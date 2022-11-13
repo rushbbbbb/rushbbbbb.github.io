4a561848dc5ed94362647108fe9ffbbd154fd808
rushb <luo1531407383@gmail.com>
2022-11-13 22:19:49 +0800

    addanimation

--- Push.bat
+++ Push.bat
@@ -1,6 +1,8 @@
 git config --global http.sslVerify "false" 
 git init
 git add .
-git commit -m "Add animation"
+set /p m=Update contest
+git commit -m %m%
 git remote add origin git@github.com:rushbbbbb/rushbbbbb.github.io.git
 git pull origin master
+pause
